8d8874b809e7ad6d5c839eeed675a90417246578
Katerou22 <xkaterou22@gmail.com>
2019-06-23 00:27:11 +0430

    Log at Command

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@
 		\InstagramAPI\Instagram::$allowDangerousWebUsageAtMyOwnRisk = TRUE;
 
 		$ig = new \InstagramAPI\Instagram(FALSE, FALSE);
+		$ig->setProxy('srsrvr.com:3128');
 		$ig->login($username, $password);
 		$feed = $ig->timeline->getUserFeed('1417570069');
 		dd($feed);
