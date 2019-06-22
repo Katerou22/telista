18a67c3ccc322e7cf2cadb239a35d15587d19535
Katerou22 <xkaterou22@gmail.com>
2019-06-23 00:09:59 +0430

    Fix Imge

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,13 @@
 
 
 	Route::get('/tester', function () {
-		$response = Telegram::setWebhook(['url' => 'https://noha.jostana.com/bot/message']);
-		dd($response);
 
-		$original = new imagick('sss.jpg');
-		dd($original);
-		$acc = \App\Account::whereHas('photos');
-		dd($acc);
-
-		//		return $acc->photos;
-
-		//		dd($acc->instagram_id);
-		$username = 'therealktr22';
-		$password = '---LOL---';
-		\InstagramAPI\Instagram::$allowDangerousWebUsageAtMyOwnRisk = TRUE;
-
-		$ig = new \InstagramAPI\Instagram(FALSE, FALSE);
-		$ig->login($username, $password);
-		dd($ig->people->getInfoById($acc->instagram_id)->getUser()->getIsPrivate());
-		$feed = $ig->timeline->getUserFeed($acc->instagram_id);
-		dd($feed->getItems()[ 0 ]->getUser()->getFollowerCount());
-		dd($feed->getItems()[ 0 ]->getImageVersions2()->getCandidates()[ 0 ]->getUrl());
+	});
+	Route::get('/users', function () {
+		return \App\User::all();
+	});
+	Route::get('/accounts', function () {
+		return \App\Account::all();
 	});
 	Route::any('/bot/message', 'BotController@message');
 	Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
