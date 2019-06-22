e7ec4213cd3f6eb1aa23b246164da551838e3f62
Katerou22 <xkaterou22@gmail.com>
2019-06-22 19:14:00 +0430

    Fix Telista

--- config/telegram.php
+++ config/telegram.php
@@ -1,61 +1,61 @@
 <?php
 
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Telegram Bot API Access Token [REQUIRED]
-    |--------------------------------------------------------------------------
-    |
-    | Your Telegram's Bot Access Token.
-    | Example: 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
-    |
-    | Refer for more details:
-    | https://core.telegram.org/bots#botfather
-    |
-    */
-    'bot_token' => env('TELEGRAM_BOT_TOKEN', '541002153:AAFJg7ZJ5xsPMukYPHuDjrP1jQTTrI7a7Mo'),
+	return [
+		/*
+		|--------------------------------------------------------------------------
+		| Telegram Bot API Access Token [REQUIRED]
+		|--------------------------------------------------------------------------
+		|
+		| Your Telegram's Bot Access Token.
+		| Example: 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
+		|
+		| Refer for more details:
+		| https://core.telegram.org/bots#botfather
+		|
+		*/
+		'bot_token'           => env('TELEGRAM_BOT_TOKEN', '869005432:AAEOoDoPvfq7ivrBMQx8WyReZk4nuRzYK7o'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Asynchronous Requests [Optional]
-    |--------------------------------------------------------------------------
-    |
-    | When set to True, All the requests would be made non-blocking (Async).
-    |
-    | Default: false
-    | Possible Values: (Boolean) "true" OR "false"
-    |
-    */
-    'async_requests' => env('TELEGRAM_ASYNC_REQUESTS', false),
+		/*
+		|--------------------------------------------------------------------------
+		| Asynchronous Requests [Optional]
+		|--------------------------------------------------------------------------
+		|
+		| When set to True, All the requests would be made non-blocking (Async).
+		|
+		| Default: false
+		| Possible Values: (Boolean) "true" OR "false"
+		|
+		*/
+		'async_requests'      => env('TELEGRAM_ASYNC_REQUESTS', FALSE),
 
-    /*
-    |--------------------------------------------------------------------------
-    | HTTP Client Handler [Optional]
-    |--------------------------------------------------------------------------
-    |
-    | If you'd like to use a custom HTTP Client Handler.
-    | Should be an instance of \Telegram\Bot\HttpClients\HttpClientInterface
-    |
-    | Default: GuzzlePHP
-    |
-    */
-    'http_client_handler' => null,
+		/*
+		|--------------------------------------------------------------------------
+		| HTTP Client Handler [Optional]
+		|--------------------------------------------------------------------------
+		|
+		| If you'd like to use a custom HTTP Client Handler.
+		| Should be an instance of \Telegram\Bot\HttpClients\HttpClientInterface
+		|
+		| Default: GuzzlePHP
+		|
+		*/
+		'http_client_handler' => NULL,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Register Telegram Commands [Optional]
-    |--------------------------------------------------------------------------
-    |
-    | If you'd like to use the SDK's built in command handler system,
-    | You can register all the commands here.
-    |
-    | The command class should extend the \Telegram\Bot\Commands\Command class.
-    |
-    | Default: The SDK registers, a help command which when a user sends /help
-    | will respond with a list of available commands and description.
-    |
-    */
-    'commands' => [
-        Telegram\Bot\Commands\HelpCommand::class,
-    ],
-];
+		/*
+		|--------------------------------------------------------------------------
+		| Register Telegram Commands [Optional]
+		|--------------------------------------------------------------------------
+		|
+		| If you'd like to use the SDK's built in command handler system,
+		| You can register all the commands here.
+		|
+		| The command class should extend the \Telegram\Bot\Commands\Command class.
+		|
+		| Default: The SDK registers, a help command which when a user sends /help
+		| will respond with a list of available commands and description.
+		|
+		*/
+		'commands'            => [
+			Telegram\Bot\Commands\HelpCommand::class,
+		],
+	];
